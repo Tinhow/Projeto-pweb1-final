51766d79a97514a6977bcda11a2d69bdf5b46eae
Geraldo Neto <netogeraldo209@gmail.com>
2023-07-06 11:39:27 -0300

    reestilizar página e fix em menu

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { ComponentesAngularModule } from '../componentes-angular/componentes-angular.module';
 import { RodapeComponent } from './rodape/rodape.component';
-
+import { Router } from '@angular/router';
 
 
 @NgModule({
--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  botaoHome = 'FITRACKER';
+  botaoTable = 'Listar Usuários - Tabela';
+  botaoSign= 'Cadastrar Usuário';
 
+  constructor(
+    private rotaAtual: ActivatedRoute,
+    private roteador: Router,
+  ){
+    
+  }
+  goHome(): void {
+    if(this.botaoHome === 'FITRACKER'){
+      this.roteador.navigate(['/homePage']);
+    }}
+  goTable(): void{
+    if(this.botaoTable === 'Listar Usuários - Tabela'){
+      this.roteador.navigate(['/listagemUsers'])
+    }
+  }
+  goSign(): void{
+    if(this.botaoSign === 'Cadastrar Usuário'){
+      this.roteador.navigate(['/cadastro'])
+
+
+    }
+  }
 }
