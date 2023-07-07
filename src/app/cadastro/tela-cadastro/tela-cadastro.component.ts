fcd22310b6b3c8dfcdcd1b67331c3c8aa35e773d
Walter <walterfilho1000@gmail.com>
2023-07-07 11:17:56 -0300

    subindo atualizado para o spring

--- src/app/cadastro/tela-cadastro/tela-cadastro.component.ts
+++ src/app/cadastro/tela-cadastro/tela-cadastro.component.ts
@@ -1,9 +1,99 @@
-import { ImensageService } from './../../shared/services/imensage.service';
+// import { ImensageService } from './../../shared/services/imensage.service';
+// import { Component } from '@angular/core';
+// import { FormControl } from '@angular/forms';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { Usuario } from 'src/app/shared/model/Usuario';
+// //import { UsuarioFirestoreService } from 'src/app/shared/services/usuario-firestore.service';
+// import { UsuarioService } from 'src/app/shared/services/usuario.service';
+
+// @Component({
+//   selector: 'app-tela-cadastro',
+//   templateUrl: './tela-cadastro.component.html',
+//   styleUrls: ['./tela-cadastro.component.scss']
+// })
+// export class TelaCadastroComponent {
+//   usuario: Usuario;
+//   estahCadastrando = true;
+//   botaoSalvar = 'Cadastrar';
+
+//   nome = new FormControl();
+//   email = new FormControl();
+//   senha = new FormControl();
+//   cpf = new FormControl();
+//   idade = new FormControl();
+//   hide = true;
+
+//   constructor(
+//     private rotaAtual: ActivatedRoute,
+//     private roteador: Router,
+//     //private usuarioService: UsuarioFirestoreService
+//     private usuarioService: UsuarioService,
+//     private imensageService: ImensageService
+//   ) {
+//     this.usuario = new Usuario();
+//     const idParaEdicao = this.rotaAtual.snapshot.paramMap.get('id');
+//     if (idParaEdicao) {
+
+//       this.estahCadastrando = false;
+//       this.botaoSalvar = 'Salvar';
+//       this.usuarioService.bucarId(Number(idParaEdicao)).subscribe(
+//         usuario => {
+//           if (usuario) {
+//             this.usuario = usuario;
+//           }
+//         }
+//       );
+//     }
+//   }
+
+//   cadastrar(): void {
+//     if (this.botaoSalvar === 'Salvar') {
+//       this.usuarioService.cadastrar(this.usuario).subscribe();
+//       console.log(this.usuario);
+//       this.imensageService.sucesso('Usuário editado com sucesso!')
+//       this.roteador.navigate(['/homePage']);
+//     } else {
+//       this.usuarioService.editar(this.usuario).subscribe(
+//         (usuarioEditado: Usuario) => {
+//           this.usuario = usuarioEditado;
+//           this.imensageService.sucesso('Usuário cadastrado com sucesso!')
+//           this.roteador.navigate(['/homePage']);
+//         },
+//         (error: any) => {
+//           console.error(error);
+//         }
+//       );
+//     }
+//   }
+
+
+//   getNameErrorMessage(): string {
+//     return 'Por favor, forneça seu nome.';
+//   }
+
+//   getEmailErrorMessage(): string {
+//     return 'Por favor, forneça um email válido.';
+//   }
+
+//   getSenhaErrorMessage(): string {
+//     return 'Por favor, forneça uma senha válida.';
+//   }
+
+//   getCPFErrorMessage(): string {
+//     return 'Por favor, forneça um CPF válido.';
+//   }
+
+//   getIdadeErrorMessage(): string {
+//     return 'Por favor, forneça uma idade válida.';
+//   }
+// }
+
+
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Usuario } from 'src/app/shared/model/Usuario';
-//import { UsuarioFirestoreService } from 'src/app/shared/services/usuario-firestore.service';
+import { ImensageService } from 'src/app/shared/services/imensage.service';
 import { UsuarioService } from 'src/app/shared/services/usuario.service';
 
 @Component({
@@ -25,18 +115,17 @@ export class TelaCadastroComponent {
 
   constructor(
     private rotaAtual: ActivatedRoute,
-    private roteador: Router,
-    //private usuarioService: UsuarioFirestoreService
+     private roteador: Router,
+     //private usuarioService: UsuarioFirestoreService
     private usuarioService: UsuarioService,
     private imensageService: ImensageService
   ) {
     this.usuario = new Usuario();
     const idParaEdicao = this.rotaAtual.snapshot.paramMap.get('id');
     if (idParaEdicao) {
-
       this.estahCadastrando = false;
       this.botaoSalvar = 'Salvar';
-      this.usuarioService.bucarId(Number(idParaEdicao)).subscribe(
+      this.usuarioService.buscarId(Number(idParaEdicao)).subscribe(
         usuario => {
           if (usuario) {
             this.usuario = usuario;
@@ -48,14 +137,23 @@ export class TelaCadastroComponent {
 
   cadastrar(): void {
     if (this.botaoSalvar === 'Salvar') {
-      this.usuarioService.cadastrar(this.usuario).subscribe();
-      console.log(this.usuario);
-      this.imensageService.sucesso('Usuário editado com sucesso!')
-      this.roteador.navigate(['/homePage']);
-    } else {
       this.usuarioService.editar(this.usuario).subscribe(
         (usuarioEditado: Usuario) => {
           this.usuario = usuarioEditado;
+          this.imensageService.sucesso('Usuário editado com sucesso!')
+          console.log(this.usuario);
+          this.roteador.navigate(['/homePage']);
+        },
+        (error: any) => {
+          this.imensageService.erro('Erro ao editar usuário!')
+          console.error(error);
+        }
+      );
+    } else {
+      this.usuarioService.cadastrar(this.usuario).subscribe(
+        (usuarioCadastrado: Usuario) => {
+          this.usuario = usuarioCadastrado;
+          console.log(this.usuario);
           this.imensageService.sucesso('Usuário cadastrado com sucesso!')
           this.roteador.navigate(['/homePage']);
         },
@@ -65,7 +163,6 @@ export class TelaCadastroComponent {
       );
     }
   }
-
 
   getNameErrorMessage(): string {
     return 'Por favor, forneça seu nome.';
--- src/app/shared/services/usuario.service.ts
+++ src/app/shared/services/usuario.service.ts
@@ -1,35 +1,113 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Usuario } from '../model/Usuario';
+// import { Observable } from 'rxjs';
+// import { throwError } from 'rxjs';
+// import { catchError } from 'rxjs/operators';
+// import { map } from 'rxjs/operators';
+// import { switchMap } from 'rxjs/operators';
+// import { of , defer } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UsuarioService {
+
+//   URL_USUARIOS = 'http://localhost:8080/usuarios';
+//   //URL_USUARIOS = 'http://localhost:3000/usuarios';
+
+//   botaoSalvar = 'Salvar'
+
+//   constructor(private httpClient: HttpClient) {
+//   }
+
+//   listar(): Observable<Usuario[]>{
+//     return this.httpClient.get<Usuario[]>(this.URL_USUARIOS);
+//   }
+
+//   cadastrar(usuario: Usuario): Observable<Usuario> {
+//     if (!usuario || !usuario.cpf) {
+//       return defer(() => {
+//         throw new Error('Por favor, forneça um CPF válido.');
+//       });
+//     }
+//     return this.listar().pipe(
+//       map((usuarios: Usuario[]) => {
+//         const cpfExists = usuarios.some(u => u.cpf === usuario.cpf);
+//         if (cpfExists) {
+//           throw new Error('CPF já cadastrado.');
+//         }
+//         return usuario;
+//       }),
+//       catchError((error: any) => {
+//         throw error;
+//       }),
+//       switchMap((usuario: Usuario) => {
+//         return this.httpClient.post<Usuario>(this.URL_USUARIOS, usuario);
+//       })
+//     );
+//   }
+
+
+
+
+//   excluir(id: number): Observable<Usuario> {
+//     return this.httpClient.delete<Usuario>(`${this.URL_USUARIOS}/${id}`)
+//   }
+
+//   editar(usuario: Usuario): Observable<Usuario> {
+//     return this.httpClient.put<Usuario>(`${this.URL_USUARIOS}/${usuario.id}`, usuario);
+//   }
+
+//   bucarId(id: number): Observable<Usuario> {
+//     return this.httpClient.get<Usuario>(`${this.URL_USUARIOS}/${id}`);
+//   }
+
+//   logar(usuario: Usuario): Observable<Usuario> {
+//     return this.listar().pipe(
+//       map((usuarios: Usuario[]) => {
+//         const usuarioEncontrado = usuarios.find(u => u.email === usuario.email && u.senha === usuario.senha);
+//         if (usuarioEncontrado) {
+//           return usuarioEncontrado;
+//         } else {
+//           throw new Error('Usuário não encontrado');
+//         }
+//       }),
+//       catchError((error: any) => {
+//         throw error;
+//       })
+//     );
+//   }
+
+//   getUsuarioByCPF(cpf: string): Observable<Usuario> {
+//     const url = `${this.URL_USUARIOS}?cpf=${cpf}`;
+//     return this.httpClient.get<Usuario>(url);
+//   }
+
+// }
+
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Usuario } from '../model/Usuario';
-import { Observable } from 'rxjs';
-import { throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { map } from 'rxjs/operators';
-import { switchMap } from 'rxjs/operators';
-import { of , defer } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-
   URL_USUARIOS = 'http://localhost:8080/usuarios';
-  //URL_USUARIOS = 'http://localhost:3000/usuarios';
 
-  botaoSalvar = 'Salvar'
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) {
-  }
-
-  listar(): Observable<Usuario[]>{
+  listar(): Observable<Usuario[]> {
     return this.httpClient.get<Usuario[]>(this.URL_USUARIOS);
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario> {
     if (!usuario || !usuario.cpf) {
-      return defer(() => {
-        throw new Error('Por favor, forneça um CPF válido.');
-      });
+      return throwError('Por favor, forneça um CPF válido.');
     }
     return this.listar().pipe(
       map((usuarios: Usuario[]) => {
@@ -39,24 +117,42 @@ export class UsuarioService {
         }
         return usuario;
       }),
-      catchError((error: any) => {
-        throw error;
-      }),
       switchMap((usuario: Usuario) => {
         return this.httpClient.post<Usuario>(this.URL_USUARIOS, usuario);
+      }),
+      catchError((error: any) => {
+        return throwError(error);
       })
     );
   }
 
+  editar(usuario: Usuario): Observable<Usuario> {
+    if (!usuario || !usuario.cpf) {
+      return throwError('Por favor, forneça um CPF válido.');
+    }
+    return this.listar().pipe(
+      map((usuarios: Usuario[]) => {
+        const cpfExists = usuarios.some(u => u.cpf === usuario.cpf && u.id !== usuario.id);
+        if (cpfExists) {
+          throw new Error('CPF já cadastrado.');
+        }
+        return usuario;
+      }),
+      switchMap((usuario: Usuario) => {
+        return this.httpClient.put<Usuario>(`${this.URL_USUARIOS}/${usuario.id}`, usuario);
+      }),
+      catchError((error: any) => {
+        return throwError(error);
+      })
+    );
+  }
 
-
+  buscarId(id: number): Observable<Usuario> {
+    return this.httpClient.get<Usuario>(`${this.URL_USUARIOS}/${id}`);
+  }
 
   excluir(id: number): Observable<Usuario> {
     return this.httpClient.delete<Usuario>(`${this.URL_USUARIOS}/${id}`)
-  }
-
-  editar(usuario: Usuario): Observable<Usuario> {
-    return this.httpClient.put<Usuario>(`${this.URL_USUARIOS}/${usuario.id}`, usuario);
   }
 
   bucarId(id: number): Observable<Usuario> {
@@ -79,15 +175,11 @@ export class UsuarioService {
     );
   }
 
-  getUsuarioByCPF(cpf: string): Observable<Usuario> {
-    const url = `${this.URL_USUARIOS}?cpf=${cpf}`;
-    return this.httpClient.get<Usuario>(url);
-  }
 
-}
 
 
 
+}
 
 
 
